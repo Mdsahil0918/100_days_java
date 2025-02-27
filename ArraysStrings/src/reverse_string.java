79f987f9de6ff1c54e1dae6e8992e508c94b3d10
Mohammad Sahil Khan <134322190+Mdsahil0918@users.noreply.github.com>
2025-02-27 13:59:44 +0530

    Update reverse_string.java

--- ArraysStrings/src/reverse_string.java
+++ ArraysStrings/src/reverse_string.java
@@ -5,18 +5,30 @@ class reverse_string {
 			System.out.println("null in put is not accepted , please provide a input");
 		}
 	}
-public static void main(String[] args) {
-	String s="sahil";
-	isempty(s);
-	String rev="";
-	for(int i=s.length()-1;i>=0;i--) {
-		char c= s.charAt(i);
-		rev=rev+c;
+	public static String reverses(String s) {
+		String rev="";
+		for(int i=s.length()-1;i>=0;i--) {
+			char c= s.charAt(i);
+			rev=rev+c;
+		}
+		System.out.println(rev);
+		return rev;
 	}
-	System.out.println(" Reverse  of "+s+" is "+rev);
+	public static void palindrom(String s,String rev) {
+		if(s.equals(rev)) {
+			System.out.println("it is palindrom ");
+			}
+		else {
+            System.out.println("It is not a palindrome.");
+            }
+	}
 	
-	if(s.equals(rev)) {
-		System.out.println("it is palindrom ");
+	
+public static void main(String[] args) {
+	String s="color";
+	
+	isempty(s);
+	String rev=reverses(s);
+	palindrom(s,rev);
 	}
-}
 }
